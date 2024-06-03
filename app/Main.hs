67d1d3e4f563a0399893f61d7ae83ebb2421b791
Thomas Jensen <thomasmollerjensen@live.dk>
2024-06-03 02:16:56 +0200

    documented usage

--- app/Main.hs
+++ app/Main.hs
@@ -15,16 +15,19 @@ header_style = style_ (M.fromList
     ])
 
 main :: IO ()
+-- Record wildcards is used here because the line below is very scuffed
+--main = startApp (App "Skademaskinen Haskell Server" updateModel viewModel [] defaultEvents Default Nothing Off)
 main = startApp App {..}
     where 
-        initialAction = Default
-        model = "Skademaskinen Haskell Webserver"
+        initialAction = Default 
+        model = "Skademaskinen Haskell-based Webserver"
         update = updateModel
         view = viewModel
         events = defaultEvents
         subs = []
-        mountPoint = Nothing
+        mountPoint = Nothing 
         logLevel = Off
+
 
 updateModel :: Action -> Model -> Effect Action Model 
 updateModel action m = noEff (m)
