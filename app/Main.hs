66be5a49b3a791ebb7988780624feb991b2b3ae0
mast3r <thomasmollerjensen@live.dk>
2024-06-03 21:39:45 +0200

    most of the core functionality needed is added

--- app/Main.hs
+++ app/Main.hs
@@ -4,35 +4,61 @@
 import Miso
 import Miso.String
 import qualified Data.Map as M
+import System.Environment
+import JavaScript.Object.Internal as JsObj
 
-type Model = String
+-- Possible actions to be taken on the site
+data Action = Default | Redirect String deriving Show
 
-data Action = Default deriving Show
+-- The model used, placeholder for now
+type Domain = MisoString
 
-global_style = style_ (M.fromList
+-- subdomains to use for urls
+subdomains :: [String]
+subdomains = ["jupyter", "nextcloud", "api", "taoshi"]
+
+-- Styles to be reused everywhere
+global_style = (M.fromList
     [
         ("background-color", "black"),
         ("color", "white")
     ])
 
-centering = style_ (M.fromList 
+centering = (M.fromList 
     [
         ("text-align", "center")
     ])
 
-outer_margins = style_ (M.fromList 
+outer_margins = (M.fromList 
     [
         ("margin-left", "10%"),
         ("margin-right", "10%")
     ])
 
+rounded = (M.fromList 
+    [
+        ("background-color", "#cccccc"),
+        ("border", "1px solid #cccccc"),
+        ("border-radius", "5px"),
+        ("padding", "5px"),
+        ("white-space", "pre-wrap")
+    ])
+
+button = (M.fromList
+    [
+        ("border 1px solid", "#cccccc"),
+        ("border-radius", "5px"),
+        ("padding", "1px")
+
+    ])
+
 main :: IO ()
 -- Record wildcards is used here because the line below is very scuffed
 --main = startApp (App "Skademaskinen Haskell Server" updateModel viewModel [] defaultEvents Default Nothing Off)
-main = startApp App {..}
+main = do startApp App {..}
     where 
         initialAction = Default 
-        model = "Skademaskinen Haskell-based Webserver"
+        model = "skade.dev"
         update = updateModel
         view = viewModel
         events = defaultEvents
@@ -40,14 +66,35 @@ main = startApp App {..}
         mountPoint = Nothing 
         logLevel = Off
 
+-- update model
+updateModel :: Action -> String -> Effect Action String
+-- default is doing nothing
+updateModel (Default) m = noEff m
+-- on redirects, change the window location variable in the following monad
+updateModel (Redirect url) m = m <# do 
+    obj <- create
+    consoleLog (pack url)
+    set (pack "window.location.href = ") url obj >> pure Default
 
-updateModel :: Action -> Model -> Effect Action Model 
-updateModel action m = noEff (m)
-
-viewModel :: Model -> View Action
-viewModel x = div_ [ outer_margins ] 
+-- view model, most of this is just haskell representation of html
+viewModel :: String -> View Action
+viewModel domain = div_ [ style_ outer_margins ] 
     [ 
-        h1_ [ centering ] [ text (ms x) ],
+        h1_ [ style_ centering ] [ text "Skademaskinen Webserver" ],
         hr_ [],
-        a_ [ href_ "https://jupyter.skade.dev" ] [ text "Jupyter" ]
+        h2_ [ style_ centering ] [ text "Information" ],
+        div_ [style_ (M.union rounded centering)] 
+            [
+                text "  This server has been rewritten for the third time! this time in Haskell using ",
+                a_ [ href_ "https://github.com/dmjio/miso" ] [ text "Miso" ],
+                text " On this site at the moment there should only be links to webservices running\nWritten by Mast3r_waf1z"
+            ],
+        hr_ [],
+        h2_ [ style_ centering ] [ text "Services" ],
+        div_ [ style_ (M.union rounded centering) ] 
+            -- onClick (Redirect ("https://" ++ subdomain ++ ('.' : domain)))
+            (Prelude.map (\subdomain -> button_ [ id_ (pack subdomain), style_ button ] [ text (pack subdomain)]) 
+                subdomains),
+        -- TODO: use redirect action properly instead of jank
+        rawHtml (pack (Prelude.concat (Prelude.map (\subdomain -> "<script>document.getElementById('"++ subdomain ++"').onclick = _ => window.location.href = \"https://"++ subdomain ++ ('.' : domain) ++"\"</script>") subdomains)))
     ]
