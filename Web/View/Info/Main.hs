d0423a542847b27e49775d2ce59ac92d2e54f0a6
mast3r <thomasmollerjensen@live.dk>
2024-06-06 12:55:52 +0200

    changed tree

--- Web/View/Info/Main.hs
+++ Web/View/Info/Main.hs
@@ -5,27 +5,28 @@ import Web.View.Styles
 
 data InfoView = MainInfoView
 
-data Tree a = Node a (Tree a) (Tree a) | Empty deriving Show
+data Tree a = Node [Tree a] | Leaf a deriving Show
 
 instance Functor Tree where 
-    fmap f (Node a n1 n2) = Node (f a) (fmap f n1) (fmap f n2)
-    fmap f (Empty) = Empty
+    fmap f (Node ns) = Node (map (fmap f) ns)
+    fmap f (Leaf a) = Leaf (f a)
 
 instance Applicative Tree where
-    pure x = Node x Empty Empty
-    _ <*> Empty = Empty
-    Empty <*> x = Empty
-    (Node f _ _) <*> (Node x n1 n2) = Node (f x) (pure f <*> n1) (pure f <*> n2)
+    pure = Leaf
+    Leaf f <*> Leaf x = Leaf (f x)
+    Leaf f <*> Node ns = Node (map (pure f <*>) ns)
+    Node fs <*> n = Node (map (<*> n) fs)
 
 instance Monad Tree where
     return = pure
-    Empty >>= f = Empty
-    (Node x _ _) >>= f = f x
+    Leaf x >>= f = f x
+    Node ns >>= f = Node (map (>>= f) ns)
 
-testTree :: Tree Int -> Tree Int
-testTree tree = do
-    x <- tree
-    return (x+1)
+allPairs :: Tree a -> Tree b -> Tree (a, b)
+allPairs t1 t2 = do
+    x <- t1
+    y <- t2
+    return (x, y)
 
 instance View InfoView where
     html MainInfoView = [hsx|
@@ -34,11 +35,11 @@ instance View InfoView where
         <div style={rounding++centering}>
             So this website is written in haskell, this means that i can pretty easily vomit out some funny haskell code like:<br>
             <div style={codeBlock}>
-                data Tree a = Node a (Tree a) (Tree a) | Empty deriving Show
+                data Tree a = Node [Tree a] | Leaf a deriving Show
             </div>
             And get the output of this code directly on the site here, idk what i would use it for, but it seemed like a fun challenge :P<br>
             <div style={codeBlock}>
-                {Node 1 Empty Empty}
+                {Node [Leaf 1, Leaf 2, Node [Leaf 3, Leaf 4]]}
             </div>
             Or something like:
             <div style={codeBlock}>
@@ -46,14 +47,19 @@ instance View InfoView where
             </div>
             And apply it to something...
             <div style={codeBlock}>
-                [in ] fmap (+1) (Node 1 (Node 2 Empty Empty) Empty)<br>
-                [out] {fmap (+1) (Node 1 (Node 2 Empty Empty) Empty)}
+                [in ] fmap (+1) (Node [Leaf 1, Leaf 2, Node [Leaf 3, Leaf 4]])<br>
+                [out] {fmap (+1) (Node [Leaf 1, Leaf 2, Node [Leaf 3, Leaf 4]])}
             </div>
             Or a monad?
             <div style={codeBlock}>
-                [in ] testTree (Node 1 (Node 2 Empty Empty) Empty)<br>
-                [out] {testTree (Node 1 (Node 2 Empty Empty) Empty)}
+                allPairs t1 t2 = do<br>
+                    x &lt;- t1<br>
+                    y &lt;- t2<br>
+                    return (x, y)
             </div>
-            This is a very bad example of a monad... i should probably rewrite that...
+            <div style={codeBlock}>
+                [in ] allPairs (Node [Leaf 1, Leaf 2, Node [Leaf 3, Leaf 4]]) (Node [Leaf 5, Leaf 6, Node [Leaf 7, Leaf 8]])<br>
+                [out] {allPairs (Node [Leaf 1, Leaf 2, Node [Leaf 3, Leaf 4]]) (Node [Leaf 5, Leaf 6, Node [Leaf 7, Leaf 8]])}
+            </div>
         </div>
     |]
