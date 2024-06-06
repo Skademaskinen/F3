bb1392c2cfabc5c6998027c53032dbb288914ca5
Thomas Jensen <thomasmollerjensen@live.dk>
2024-06-06 02:33:58 +0200

    dynamic loading of services

--- Web/View/Main/Main.hs
+++ Web/View/Main/Main.hs
@@ -7,8 +7,6 @@ import Web.View.Styles
 -------------------------
 data MainView = MainView
 
-subdomains = ["nextcloud", "jupyter", "api", "taoshi"]
-
 --- Builders
 -------------------------
 link_image :: String -> String -> String -> Html 
@@ -27,6 +25,51 @@ render_subdomain_button subdomain = [hsx|
     <button style={button} id={subdomain} onclick="go_to(this.id)">{subdomain}</button>
 |]
 
+--- sections
+-------------------------
+sourcesSection :: Html
+sourcesSection = [hsx|
+    <hr>
+    <h2 style={centering}>Sources</h2>
+    <div style={centering++rounding}>
+        This website is written using the following software:<br>
+        {link_image "IHP" "https://ihp.digitallyinduced.com/ihp.svg" "https://ihp.digitallyinduced.com/"}
+        {link_image "Haskell" "https://wiki.haskell.org/wikiupload/4/4a/HaskellLogoStyPreview-1.png" "https://haskell.org"}
+        {link_image "Nix" "https://raw.githubusercontent.com/NixOS/nixos-artwork/53ea652ec7d8af5d21fd2b79b6c49cb39078ddfb/logo/nix-snowflake-colours.svg" "https://nixos.org"}
+        {link_image "Neovim" "https://upload.wikimedia.org/wikipedia/commons/3/3a/Neovim-mark.svg" "https://neovim.io"}
+        <br>
+        Source code: <a href="https://github.com/Skademaskinen/F3">
+            <img src="/github.svg" height="20">
+        </a>
+    </div>
+|]
+
+infoSection :: Html
+infoSection = [hsx|
+    <hr>
+    <h2 style={centering}>Information</h2>
+    <div style={rounding++centering}>
+        This is the frontend v3 of Skademaskinen, using IHP, Haskell and Nix. At the moment only links to software hosted is present.<br>
+        Written by Mast3r_waf1z
+    </div>
+|]
+
+servicesSection :: [Text] -> Html
+servicesSection subdomains = [hsx|
+    <hr>
+    <h2 style={centering}>Services</h2>
+    <div style={rounding++centering}>
+        This section contains links to services hosted on this server <br>
+        <script>
+            function go_to(id){
+                console.log(id)
+                window.location.href = "https://"+id+"."+window.location.hostname
+            }
+        </script>
+        {forEach subdomains render_subdomain_button}
+    </div>
+|]
+
 --- Main
 -------------------------
 instance View MainView where
@@ -35,35 +78,7 @@ instance View MainView where
         <div style={centering}>
             <img src={assetPath"/icon.png"} style="border-radius:50%">
         </div>
-        <hr>
-        <h2 style={centering}>Sources</h2>
-        <div style={centering++rounding}>
-            This website is written using the following software:<br>
-            {link_image "IHP" "https://ihp.digitallyinduced.com/ihp.svg" "https://ihp.digitallyinduced.com/"}
-            {link_image "Haskell" "https://wiki.haskell.org/wikiupload/4/4a/HaskellLogoStyPreview-1.png" "https://haskell.org"}
-            {link_image "Nix" "https://raw.githubusercontent.com/NixOS/nixos-artwork/53ea652ec7d8af5d21fd2b79b6c49cb39078ddfb/logo/nix-snowflake-colours.svg" "https://nixos.org"}
-            {link_image "Neovim" "https://upload.wikimedia.org/wikipedia/commons/3/3a/Neovim-mark.svg" "https://neovim.io"}
-            <br>
-            Source code: <a href="https://github.com/Skademaskinen/F3">
-                <img src="/github.svg" height="20">
-              </a>
-        </div>
-        <hr>
-        <h2 style={centering}>Information</h2>
-        <div style={rounding++centering}>
-            This is the frontend v3 of Skademaskinen, using IHP, Haskell and Nix. At the moment only links to software hosted is present.<br>
-            Written by Mast3r_waf1z
-        </div>
-        <hr>
-        <h2 style={centering}>Services</h2>
-        <div style={rounding++centering}>
-            This section contains links to services hosted on this server <br>
-            <script>
-                function go_to(id){
-                    console.log(id)
-                    window.location.href = "https://"+id+".skade.dev"
-                }
-            </script>
-            {forEach subdomains render_subdomain_button}
-        </div>
+        {infoSection}
+        {servicesSection ["nextcloud", "jupyter", "matrix", "api", "taoshi"]}
+        {sourcesSection}
     |]
