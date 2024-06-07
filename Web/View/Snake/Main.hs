59de88d78d42ff306ac98c8a2902d417a9c60134
Thomas Jensen <thomasmollerjensen@live.dk>
2024-06-07 02:38:49 +0200

    fixed bugs

--- Web/View/Snake/Main.hs
+++ Web/View/Snake/Main.hs
@@ -10,7 +10,7 @@ mainStyle :: String
 mainStyle = "background-color: #444444; border-radius: 10px;line-height:0px; width:fit-content;display:inline-block; padding:20px;border: 1px solid white;"
 
 tileStyle :: String
-tileStyle = "width:15px;height:15px;background-color:black;margin:0px;display:inline-grid;"
+tileStyle = "width:25px;height:25px;background-color:black;margin:0px;display:inline-grid;"
 
 tile :: Int -> Html
 tile id = [hsx|
@@ -20,7 +20,7 @@ tile id = [hsx|
 
 row :: Int -> Html
 row offset = [hsx|
-    {forEach [(offset*50)..49+(offset*50)] tile}
+    {forEach [(offset*20)..19+(offset*20)] tile}
     <br>
 |]
 
@@ -37,11 +37,11 @@ instance View SnakeView where
                 <p>... Some client-side jank, a mix of haskell and javascript</p><br><br>
                 <div style="text-align:left; margin-left: 5%;line-height:normal">
                     Controls<br>
-                    Start game: any of [W,A,S,D]<br>
-                    Up: W<br>
-                    Left: A<br>
-                    Down: S<br>
-                    Right: D<br>
+                    Start game: enter<br>
+                    Up: W or up<br>
+                    Left: A or left<br>
+                    Down: S or down<br>
+                    Right: D or right<br>
                     <input style="width:100px" placeholder="Speed" id="speed">
                     <input style="width:100px" placeholder="Fruit count" id="fruits">
                 </div>
@@ -53,7 +53,7 @@ instance View SnakeView where
         </div>
         <script>
             function newFruit(){
-                var coords = [Math.floor(Math.random()*50), Math.floor(Math.random()*20)]
+                var coords = [Math.floor(Math.random()*20), Math.floor(Math.random()*20)]
                 if(tiles[coords[1]][coords[0]].style.backgroundColor != "black"){
                     return newFruit()
                 }
@@ -64,7 +64,6 @@ instance View SnakeView where
 
             async function launchGame(){
                 var running = true
-                tiles.forEach(row => row.forEach(tile => tile.style.backgroundColor = "black"))
                 try{
                     while(running){
                         tail.push([x, y])
@@ -84,10 +83,11 @@ instance View SnakeView where
                             default:
                                 running = false
                         }
-                        if(tiles[y][x].style.backgroundColor == "white"){
+                        direction = next_direction
+                        if(tiles[y][x].style.backgroundColor == "red"){
                             running = false
                         }
-                        tiles[y][x].style.backgroundColor = "white"
+                        tiles[y][x].style.backgroundColor = "red"
                         var new_fruits = []
                         var score_increase = false
                         fruits.forEach(([fruit_x, fruit_y]) => {
@@ -126,8 +126,14 @@ instance View SnakeView where
                     timetd.innerHTML = time
                     var scoretd = document.createElement("td")
                     scoretd.innerHTML = score
+                    var speedtd = document.createElement("td")
+                    speedtd.innerHTML = speed
+                    var fruitstd = document.createElement("td")
+                    fruitstd.innerHTML = fruit_count
                     tr.appendChild(timetd)
                     tr.appendChild(scoretd)
+                    tr.appendChild(speedtd)
+                    tr.appendChild(fruitstd)
                     scoreboard.appendChild(tr)
                 }
             }
@@ -135,57 +141,67 @@ instance View SnakeView where
             var tiles = []
             var fruits = []
             var tail = []
-            var fruit_count = 5
+            var fruit_count = 1
             var speed = 25
-
             var score = 0
             var direction = "N"
+            var next_direction = "N"
             var x = 5
             var y = 5
+
             var row = []
-            for(var index = 0; index <= 20*50; index++){
-                if(row.length == 50){
+            for(var index = 0; index <= 20*20; index++){
+                if(row.length == 20){
                     tiles.push(row)
                     row = []
                 }
                 row.push(document.getElementById("tile-"+index))
             }
             var started = false
-            var keycodes = {87:"W",65:"A",83:"S",68:"D"}
 
             document.addEventListener('keydown', event => {
-                if(
-                    (event.keyCode == 87 && direction == "S") ||
-                    (event.keyCode == 65 && direction == "D") ||
-                    (event.keyCode == 83 && direction == "W") ||
-                    (event.keyCode == 68 && direction == "A")
-                ){
-                    return
+                switch(event.keyCode){
+                    case 13:
+                        if(!started){
+                            tiles.forEach(row => row.forEach(tile => tile.style.backgroundColor = "black"))
+                            var speed_input = document.getElementById("speed")
+                            var fruits_input = document.getElementById("fruits")
+                            if(speed_input.value != ""){
+                                speed = parseFloat(speed_input.value)
+                            }
+                            if(fruits_input.value != ""){
+                                fruit_count = parseInt(fruits_input.value)
+                            }
+                            started = !started
+                            score = 0
+                            for(var i = 0; i < fruit_count; i++){
+                                newFruit()
+                            }
+                            direction = "D"
+                            launchGame()
+                        }
+                        break
+                    case 87:
+                    case 38:
+                        if(direction != "S")
+                            next_direction = "W"
+                        break
+                    case 65:
+                    case 37:
+                        if(direction != "D")
+                            next_direction = "A"
+                        break
+                    case 83:
+                    case 40:
+                        if(direction != "W")
+                            next_direction = "S"
+                        break
+                    case 68:
+                    case 39:
+                        if(direction != "A")
+                            next_direction = "D"
+                        break
                 }
-                if(keycodes[event.keyCode] == undefined){
-                    return
-                }
-                direction = keycodes[event.keyCode]
-                console.log(direction + " Pressed")
-                if(!started){
-                    console.log("starting game!")
-                    var speed_input = document.getElementById("speed")
-                    var fruits_input = document.getElementById("fruits")
-                    if(speed_input.value != ""){
-                        speed = parseInt(speed_input.value)
-                    }
-                    if(fruits_input.value != ""){
-                        fruit_count = parseInt(fruits_input.value)
-                    }
-                    alert("Starting Game!")
-                    started = !started
-                    score = 0
-                    launchGame()
-                    for(var i = 0; i < fruit_count; i++){
-                        newFruit()
-                    }
-                }
-
             })
         </script>
         <hr>
@@ -195,6 +211,8 @@ instance View SnakeView where
                 <tr>
                     <th style="width:100px">Timestamp</th>
                     <th style="width:100px">Score</th>
+                    <th style="width:100px">Speed</th>
+                    <th style="width:100px">Fruits</th>
                 </tr>
             </table>
         </div>
